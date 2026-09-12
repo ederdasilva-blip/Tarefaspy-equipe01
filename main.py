31e32fbc18b29227d2fe5e295db5d3205729d830
Eder Andrew <eder.dasilva@pr.fecaf.com.br>
2026-09-12 09:40:25 -0300

    feat: Adiciona menu Inicial

---
+++ main.py
@@ -0,0 +1,9 @@
+print("====================")
+print("     Tarefaspy")
+print("====================")
+
+print("1 - Adicionar tarefa")
+print("2 - Listar tarefas")
+print("3 - Concluir tarefa")
+print("4 - Remover tarefa")
+print("0 - Sair")
