350d4bb3154bb98dbf957bb41f579cc4f409e03e
Rafael <rafael@gmail.com>
2026-09-12 11:19:10 -0300

    Validação menu e adicionar tarefa integrado no meu

--- tarefas.py
+++ tarefas.py
@@ -1,10 +1,12 @@
 tarefas=[]
+
 def adicionar_tarefa():
-    descricao = input("digite a descricao da tarefa:").strip()
+    descricao = input("digite a descricao da tarefa: ").strip()
 
     tarefa = ({
         "descricao": descricao,
         "concluida": False
     })
+
     tarefas.append(tarefa)
     print("tarefa adicionada com sucesso!")
