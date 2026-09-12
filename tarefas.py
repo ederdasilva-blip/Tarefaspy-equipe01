48b68b7e98a51fbe23381c7bff4866dc88682225
guilhermeRikesilva <guilherme.rikesilva@gmail.com.br>
2026-09-12 11:23:01 -0300

    concluir-tarefas

--- tarefas.py
+++ tarefas.py
@@ -8,3 +8,29 @@ def adicionar_tarefa():
     })
     tarefas.append(tarefa)
     print("tarefa adicionada com sucesso!")
+
+def concluir_tarefa():
+    if len(tarefas) ==0:
+        print("Nenhuma tarefa cadastrada.")
+    return
+
+listar_tarefa()
+
+try:
+    numero = int(
+        input("Digite o número da tarefa que deseja concluir:")
+    )
+    
+
+    indice = numero - 1
+
+    if indice < 0 or indice >= len(tarefas):
+    print("tarefa inválida.")
+return
+
+    tarefas[indice]["concluida"] = True
+
+    print("Tarefa concluída com sucesso!")
+
+except valueError:
+    print("Digite um número válido")
